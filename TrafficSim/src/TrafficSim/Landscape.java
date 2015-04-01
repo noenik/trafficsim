ae011d52476014086469899a667632f41702a57f
noenik <niklas.noem@gmail.com>
2015-04-01 11:47:19 +0200

    En start

--- TrafficSim/src/TrafficSim/Landscape.java
+++ TrafficSim/src/TrafficSim/Landscape.java
@@ -1,9 +1,81 @@
-package TrafficSim;
+package trafficsim;
+
+import java.util.ArrayList;
+import processing.core.*;
 
 /**
  *
  * @author nikla_000
  */
-public class Landscape {
-    
+public class Landscape extends PApplet {
+
+    ArrayList<Square> grid = new ArrayList<>();
+
+    public void setup() {
+
+        size(1000, 1000);
+        background(0, 255, 0);
+
+        noLoop();
+
+    }
+
+    public void draw() {
+
+        fill(200);
+        rectMode(CENTER);
+        rect(width / 2, (height / 2) - (75 / 2), 1000, 75);
+        rect((width / 2) - (75 / 2), height / 2, 75, 1000);
+
+        fill(220);
+        rect(width / 2, (height / 2) + (75 / 2), 1000, 75);
+        rect((width / 2) + (75 / 2), height / 2, 75, 1000);
+
+        line(width / 2, 0, width / 2, height);
+        line(0, height / 2, width, height / 2);
+
+        ellipseMode(CENTER);
+        fill(214);
+        ellipse(width / 2, height / 2, 400, 400);
+        fill(207);
+        ellipse(width / 2, height / 2, 250, 250);
+        fill(155);
+        ellipse(width / 2, height / 2, 100, 100);
+
+        makeGrid();
+    }
+
+    public PImage getLandscape() {
+
+        return this.get();
+
+    }
+
+    private void makeGrid() {
+
+        int dir1Color = get(490, 10);
+        int dir2Color = get(510, 10);
+        int innerRAColor = get(560, 560);
+        int outerRAColor = get(620, 620);
+        
+        rectMode(CORNER);
+        for (int x = 0; x < width; x += 10) {
+            for (int y = 0; y < height; y += 10) {
+                if (get(x+5, y+5) == dir1Color) {
+                    grid.add(new Square(x, y, 255));
+                } else if (get(x+5, y+5) == dir2Color) {
+                    grid.add(new Square(x, y, 200));
+                } else if (get(x+5, y+5) == innerRAColor) {
+                    grid.add(new Square(x, y, 150));
+                } else if (get(x+5, y+5) == outerRAColor) {
+                    grid.add(new Square(x, y, 100));
+                }
+            }
+        }
+        
+        for(Square s : grid) {
+            fill(s.getR(), 0, 0);
+            rect(s.getxStart(), s.getyStart(), 10, 10);
+        }
+    }
 }
--- TrafficSim/src/TrafficSim/Simulator.java
+++ TrafficSim/src/TrafficSim/Simulator.java
@@ -1,4 +1,4 @@
-package TrafficSim;
+package trafficsim;
 
 import processing.core.*;
 
@@ -8,14 +8,36 @@ import processing.core.*;
  */
 public class Simulator extends PApplet {
 
+    Landscape landscape;
+    PImage landscapeImage;
+    PImage car;
+    float x = 0;
+
     @Override
     public void setup() {
-        size(400, 400);
+        size(1000, 1000);
+        car = loadImage("graphics/bil1.png");
+        car.resize(100, 0);
+//        noLoop();
     }
 
     @Override
     public void draw() {
-        
+        image(landscape.getLandscape(), 0, 0);
+
+        image(car, x, 500);
+
+        if (x > 1100) {
+            x = 0;
+        } else {
+            x++;
+        }
+    }
+
+    public void setLandscape(Landscape landscape) {
+
+        this.landscape = landscape;
+
     }
 
 }
