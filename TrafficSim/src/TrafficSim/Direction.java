5c2d74f17c3e3a50ae202f132fd939144e23a8c1
noenik <niklas.noem@gmail.com>
2015-04-03 11:08:40 +0200

    Square og direction class

---
+++ TrafficSim/src/TrafficSim/Direction.java
@@ -0,0 +1,9 @@
+package TrafficSim;
+
+/**
+ *
+ * @author nikla_000
+ */
+public enum Direction {
+    NORTH, SOUTH, WEST, EAST;
+}
---
+++ TrafficSim/src/TrafficSim/Square.java
@@ -0,0 +1,72 @@
+package TrafficSim;
+
+/**
+ *
+ * @author nikla_000
+ */
+public class Square {
+
+    private final float xStart;
+    private final float yStart;
+    private final int colNum;
+    private final int rowNum;
+    private int size;
+    private Direction direction;
+
+    private Vehicle occupant;
+
+    public Square(float x, float y, int colNum, int rowNum, int size, Direction dir) {
+
+        xStart = x;
+        yStart = y;
+        direction = dir;
+        this.size = size;
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+
+    }
+
+    public boolean hasPoint(float x, float y) {
+
+        boolean withinX = x > xStart && x < (xStart + size);
+        boolean withinY = y > yStart && y < (yStart + size);
+
+        return withinX && withinY;
+
+    }
+
+    public void setOccupant(Vehicle v) {
+        occupant = v;
+    }
+
+    public float getxStart() {
+        return xStart;
+    }
+
+    public float getyStart() {
+        return yStart;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getColNum() {
+        return colNum;
+    }
+
+    public int getRowNum() {
+        return rowNum;
+    }
+
+    public Vehicle getOccupant() {
+        return occupant;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+    
+    
+
+}
